bf2514b3fc289acf4f13ed4e4dba45b1784c3f59
AshenSachinthana <dasachinthana@gmail.com>
2025-06-21 09:50:48 +0530

    Add GitHub Pages deployment workflow and update Vite config base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/AshenSachinthana/portfolio-web.git',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
